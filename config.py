941b01c930ff91486e4f4d60f9e6710c65397b55
Bruh <m29752885@gmail.com>
2026-01-04 00:16:08 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+# config.py
+TOKEN = "MTQ1NzA1Mzk0MjMzMTYwOTA5OA.GtJDP8.nHcV6Z4sj85d7NUY3bF12kXqznLXpt6V_mMgYE"  # Token bot Discord
+CHANNEL_AI = 1457054883818508380     # ID channel tempat bot akan merespon
